ed944ae469b15ed4b5da5162e5eb51b6a58d0ce7
Pablo Huinac <pablohuinac@hotmail.com>
2016-11-18 10:59:18 -0600

    modificacion models, views

--- biblioteca/views.py
+++ biblioteca/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
+from django.shortcuts import render, get_object_or_404
+from django.utils import timezone
+
+from django.shortcuts import redirect
 # Create your views here.
+
+def post_detail(request, pk):
+        post = get_object_or_404(Postear, pk=pk)
+        return render(request, 'blog/detalle_articulo.html', {'Postear': post})
